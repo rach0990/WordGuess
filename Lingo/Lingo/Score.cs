2b0728768cd55b88dc72a85f172f8293b630408a
Rachael <83975479+rach0990@users.noreply.github.com>
2021-10-13 15:47:33 +0100

    Created score functionality across modes

--- Lingo/Lingo/Score.cs
+++ Lingo/Lingo/Score.cs
@@ -10,39 +10,50 @@ namespace Lingo
 
         public void EasyScore(string currentGuessBox)
         {
-           
-
+                     
             switch (currentGuessBox) 
             {
-                
+
                 case "guess1":
+                case "medGuess1":
+                case "hardGuess1":               
                      score+= 5;
                     break;
 
                 case "guess2":
+                case "medGuess2":
+                case "hardGuess2":
                     score+=4;
                     break;
 
                 case "guess3":
+                case "medGuess3":
+                case "hardGuess":
                     score+=3;
                     break;
 
                 case "guess4":
+                case "medGuess4":
+                case "hardGuess4":
                     score+=2;
                     break;
 
                 case "guess5":
+                case "medGuess5":
+                case "hardGuess5":
                     score+=1;
                     break;
 
                 default:
                    
                     break;   
-            }
-            
+            }                       
         }
 
-      
+        public int Zero()
+        {
+           return score = 0;
+        }
     }
 }
 
